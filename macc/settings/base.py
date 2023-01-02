6cf5ad1d1b0386d7b58b0917e29bff9db7aaee8b
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2023-01-02 11:19:45 -0300

    update

--- macc/settings/base.py
+++ macc/settings/base.py
@@ -154,9 +154,12 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
