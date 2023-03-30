577af51d817e42ececba9c21a7890e7a9ea3c1bc
Aline <line.asramos@gmail.com>
2023-03-30 00:37:24 -0300

    Edit Translation admin for better usage

--- corpus/admin.py
+++ corpus/admin.py
@@ -2,12 +2,31 @@ from django.contrib import admin
 
 from .models import Author, Collection, Translator, Work, Place, Publisher, Translation, OriginalFragment, TranslatedFragment
 
+
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ('work_code', 'work_title', 'code', 'title',)
+    list_display_links = ('code',)
+
+    list_select_related = ('work',)
+    search_fields = ('work__title', 'title',)
+
+    ordering = ('code',)
+
+    @admin.display
+    def work_code(self, obj):
+        return obj.work.code
+
+    @admin.display
+    def work_title(self, obj):
+        return obj.work.title
+
+
 admin.site.register(Author)
 admin.site.register(Translator)
 admin.site.register(Work)
 admin.site.register(Place)
 admin.site.register(Publisher)
 admin.site.register(Collection)
-admin.site.register(Translation)
+admin.site.register(Translation, TranslationAdmin)
 admin.site.register(OriginalFragment)
 admin.site.register(TranslatedFragment)
